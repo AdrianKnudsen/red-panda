67e4b1ecebec58b8c133e0944adb1ca4fc9e27b8
FeyzaO-H04 <feyzaoh04.kodehode@gmail.com>
2023-11-30 13:13:50 +0100

    Created game page og news page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { LayoutBase } from './Pages/LayoutBase'
 import { LandingPage } from './Pages/LandingPage'
+import { GamePage } from "./Pages/GamesPage/gamePage"
+import { NewsPage } from "./Pages/NewsPage/newsPage"
 import './App.css'
 import { 
   BrowserRouter,
@@ -14,8 +16,10 @@ export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LayoutBase />}>
-          <Route index element={<LandingPage />}/>
+        <Route path='/' element={<LayoutBase />} >
+          <Route index element={<LandingPage />} />
+          <Route path='/game' element={<GamePage />} />
+          <Route path='/new' element={<NewsPage />} />
           <Route path='*' element={<h1>404 Not Found</h1>}/>
         </Route>
       </Routes>
