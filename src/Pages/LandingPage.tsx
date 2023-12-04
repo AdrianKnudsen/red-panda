f23e5366aa9eae72b6d100a8b7b4fbf1fdc2770c
FeyzaO-H04 <feyzaoh04.kodehode@gmail.com>
2023-12-04 09:50:28 +0100

    a little change

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,11 +1,9 @@
-import { GameCard } from "../Components/GameCard/gameCard";
 
 
 export function LandingPage() {
   return(
     <main>
       <h1>Main</h1>
-      <GameCard />
     </main>
   )
 }
