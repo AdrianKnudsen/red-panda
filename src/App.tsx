1494f7a4efb5b860d600a7559abe4afbfb675cdd
kristinahb06kodehode <kristinah.b06.kodehode@gmail.com>
2023-12-04 19:29:14 +0100

    Kristina Commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import { LayoutBase } from './Pages/LayoutBase'
-import { LandingPage } from './Pages/LandingPage'
-import './App.css'
-import { 
-  BrowserRouter,
-  Routes,
-  Route, 
-} from 'react-router-dom'
-
-
+import { LayoutBase } from "./Pages/LayoutBase";
+import { LandingPage } from "./Pages/LandingPage";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export function App() {
-  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LayoutBase />}>
-          <Route index element={<LandingPage />}/>
-          <Route path='*' element={<h1>404 Not Found</h1>}/>
+        <Route path="/" element={<LayoutBase />}>
+          <Route index element={<LandingPage />} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
-
-
--- src/Pages/LayoutBase.tsx
+++ src/Pages/LayoutBase.tsx
@@ -1,17 +1,16 @@
-import { Outlet } from "react-router-dom"
-
-
-
-export function LayoutBase(){
+import { Outlet } from "react-router-dom";
+import { NavBar } from "../Components/NavBar/NavBar";
+import { Aside } from "../Components/Aside/Aside";
+export function LayoutBase() {
   return (
     <>
-      <header> Header </header>
-
+      <NavBar />
+      <Aside />
       <div>
         <Outlet />
       </div>
 
-      <footer> Footer </footer>
+      <footer></footer>
     </>
-  )
+  );
 }
