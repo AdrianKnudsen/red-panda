2def75b45553523702aabbfe643f66a4b5339152
kristinahb06kodehode <kristinah.b06.kodehode@gmail.com>
2023-12-05 14:52:31 +0100

    Navbar to layout to see

--- src/Pages/LayoutBase.tsx
+++ src/Pages/LayoutBase.tsx
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router-dom";
 import HeroSection from "../Components/HeroSection/heroSection";
-
+import { NavBar } from "../Components/NavBar/NavBar";
 export function LayoutBase() {
   return (
     <>
+      <NavBar />
       <HeroSection />
 
       <div>
         <Outlet />
       </div>
 
-      <footer> Footer </footer>
+      <footer> </footer>
     </>
   );
 }
