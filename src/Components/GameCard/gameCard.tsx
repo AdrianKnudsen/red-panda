ddb9f27247f1dc671b23c76cdd7e26c90cee14f5
AdrianKnudsen <adrian@knudsen.no>
2023-12-06 13:10:15 +0100

    made home page, and changed some css on game cards
    Added components to home/landing page, added style to home/landing page, changed some css to the gamecard to make it scale.

--- src/Components/GameCard/gameCard.tsx
+++ src/Components/GameCard/gameCard.tsx
@@ -1,22 +1,21 @@
-import styles from "./gameCard.module.css"
+import styles from "./gameCard.module.css";
 
-
-export function GameCard(){
-
-
-  return(
+export function GameCard() {
+  return (
     <div className={styles.cardContainer}>
-      
       <div className={styles.cardContainerInner}>
-        <img className={styles.cardImage} src="./card.image.png" alt="card-image" />
+        <img
+          className={styles.cardImage}
+          src="./card.image.png"
+          alt="card-image"
+        />
         <h2 className={styles.cardName}>Red-panda: Warfare</h2>
-        <h3 className={styles.cardAgeRating}>ALL</h3>
+        <h3 className={styles.gameSystem}>ALL</h3>
         <div className={styles.cardPriceBuy}>
           <p className={styles.cardPrice}>$20.00</p>
           <button className={styles.cardButton}>Buy</button>
         </div>
       </div>
-   
     </div>
-  )
+  );
 }
