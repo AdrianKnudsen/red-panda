99adb21207633bde47efbdcb2e9d8778c5b796b5
EirBec <eirbec.kodehode@gmail.com>
2023-12-06 13:31:31 +0100

    HUE Co-authored-by: sindrekodehode <sindrekodehode@users.noreply.github.com>

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,9 +1,10 @@
-
+import { GameCard } from "../Components/GameCard/gameCard";
 
 export function LandingPage() {
   return (
     <main>
       <h1>Main</h1>
+      <GameCard />
     </main>
   );
 }
